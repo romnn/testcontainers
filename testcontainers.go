0ac7fb816041af0c2849575c1ed93a912b228855
romnnn <roman.dahm@gmx.de>
2020-09-06 11:32:59 +0200

    Bump version: 0.1.11 → 0.1.12

--- testcontainers.go
+++ testcontainers.go
@@ -3,7 +3,7 @@ package testcontainers
 import "sync"
 
 // Version is incremented using bump2version
-const Version = "0.1.11"
+const Version = "0.1.12"
 
 // ClientMux ...
 var ClientMux sync.Mutex
